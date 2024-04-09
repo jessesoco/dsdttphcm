3195e23ce8655997b867d309e3224c46cefab1e4
MARKETROLE\Jesse <jesse.soco1307@gmail.com>
2024-04-09 08:41:25 +0800

    Commit source

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dsdttphcm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
